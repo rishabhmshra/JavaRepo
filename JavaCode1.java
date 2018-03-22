44986ee9d95d03ec5c9dfaca50e14e7c3c3a314c
rishabh <rishabh.mishra@tothenew.com>
2018-03-22 14:40:02 +0530

    delete file

--- JavaCode1.java
+++
@@ -1,7 +0,0 @@
-class JavaCode{
-
-public static void main(String... args){
-System.out.println("Java program for Jenkins!!");
-}
-
-}
--- src/FirstCode.java
+++ src/FirstCode.java
@@ -1,4 +1,10 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstCode {
-
+public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver", "/home/rishabh/Downloads/chromedriver");
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://www.google.com");
+}
 }
