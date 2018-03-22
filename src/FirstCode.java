3d7180a829a086f99c4ecd765a6eb8365be4581e
rishabh <rishabh.mishra@tothenew.com>
2018-03-22 14:51:45 +0530

    commit

--- src/FirstCode.java
+++ src/FirstCode.java
@@ -1,10 +1,6 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FirstCode {
 public static void main(String[] args) {
-	System.setProperty("webdriver.chrome.driver", "/home/rishabh/Downloads/chromedriver");
-	WebDriver driver=new ChromeDriver();
-	driver.get("https://www.google.com");
+	System.out.println("");
 }
 }
